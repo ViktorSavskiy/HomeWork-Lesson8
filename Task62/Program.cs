12baa638d7ab95a28bea6a6228f8b2d10cc8943f
Viktor <savskij@bk.ru>
2022-09-17 22:39:21 +0300

    Task62

---
+++ Task62/Program.cs
@@ -0,0 +1,46 @@
+﻿int row = 4; 
+int columns = row;
+int[,] matrix = new int[row, columns];
+ 
+int[,] Matrix = CreateNewArray(matrix);
+ 
+PrintArray(Matrix);
+Console.WriteLine("");
+
+
+ 
+void PrintArray(int[,] matr)
+{
+ 
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+Console.Write(matr[i, j] + " ");
+}
+Console.WriteLine("");
+}
+ 
+}
+ 
+int[,] CreateNewArray(int[,] matr)
+{
+int num = 1;
+int i = 0;
+int j = 0;
+ 
+while (num <= row * columns)
+{
+matrix[i, j] = num;
+if (i <= j + 1 && i + j < row - 1)
+++j;
+else if (i < j && i + j >= row - 1)
+++i;
+else if (i >= j && i + j > row - 1)
+--j;
+else
+--i;
+++num;
+}
+return matrix;
+}
